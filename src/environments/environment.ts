cf08955ef1fdc8b8162757e5653eec1d3a24da6e
Benoit Lavenier <benoit.lavenier@e-is.pro>
2022-04-15 18:30:58 +0200

    Prepare 1.24.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -29,7 +29,7 @@ export const environment = Object.freeze(<AppEnvironment>{
   // TODO: make this works
   //offline: true,
 
-  peerMinVersion: '1.21.0',
+  peerMinVersion: '1.24.0',
 
   // Not need during DEV
   //checkAppVersionIntervalInSeconds: 0,
