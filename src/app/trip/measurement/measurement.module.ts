aa8a55d567b74e9023f646ae7f6768a7633a97ed
Benoit Lavenier <benoit.lavenier@e-is.pro>
2022-06-07 09:26:50 +0200

    [fix] Fix compilation: add measurement module to landed-trip module

--- src/app/trip/measurement/measurement.module.ts
+++ src/app/trip/measurement/measurement.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { MeasurementsForm } from './measurements.form.component';
 import { AppReferentialModule } from '@app/referential/referential.module';
-import { AppDataModule } from '@app/data/data.module';
-import { SocialModule } from '@sumaris-net/ngx-components';
 import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { AppCoreModule } from '@app/core/core.module';
