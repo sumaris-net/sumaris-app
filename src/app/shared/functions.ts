8161d6791fd9b39ae10e0bdb59a93c9200102ec8
Benoit Lavenier <benoit.lavenier@e-is.pro>
2022-06-09 16:19:04 +0200

    [enh] Add sampling ratio field

--- src/app/shared/functions.ts
+++ src/app/shared/functions.ts
@@ -30,3 +30,63 @@ export function roundHalfUp(value: number|string, maxDecimals: number): number {
   const divider = maxDecimals ? Math.pow(10, maxDecimals) : 1;
   return Math.trunc(+value * divider + 0.5) / divider;
 }
+
+// Compare two items
+export function equals(item1, item2) {
+
+  // Get the object type
+  const itemType = Object.prototype.toString.call(item1);
+
+  // If an object or array, compare recursively
+  if (['[object Array]', '[object Object]'].indexOf(itemType) >= 0) {
+    if (!arrayEquals(item1, item2)) return false;
+  }
+
+  // Otherwise, do a simple comparison
+  else {
+
+    // If the two items are not the same type, return false
+    if (itemType !== Object.prototype.toString.call(item2)) return false;
+
+    // Else if it's a function, convert to a string and compare
+    // Otherwise, just compare
+    if (itemType === '[object Function]') {
+      if (item1.toString() !== item2.toString()) return false;
+    } else {
+      if (item1 !== item2) return false;
+    }
+  }
+}
+
+export function arrayEquals<T>(value: T[], other:T[]): boolean {
+
+  // Get the value type
+  const type = Object.prototype.toString.call(value);
+
+  // If the two objects are not the same type, return false
+  if (type !== Object.prototype.toString.call(other)) return false;
+
+  // If items are not an object or array, return false
+  if (['[object Array]', '[object Object]'].indexOf(type) < 0) return false;
+
+  // Compare the length of the length of the two items
+  const valueLen = type === '[object Array]' ? value.length : Object.keys(value).length;
+  const otherLen = type === '[object Array]' ? other.length : Object.keys(other).length;
+  if (valueLen !== otherLen) return false;
+
+  // Compare properties
+  if (type === '[object Array]') {
+    for (let i = 0; i < valueLen; i++) {
+      if (equals(value[i], other[i]) === false) return false;
+    }
+  } else {
+    for (let key in value) {
+      if (value.hasOwnProperty(key)) {
+        if (equals(value[key], other[key]) === false) return false;
+      }
+    }
+  }
+
+  // If nothing failed, return true
+  return true;
+}
---
+++ src/app/shared/material/sampling-ratio/material.sampling-ratio.ts
@@ -0,0 +1,275 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, forwardRef, Input, OnDestroy, OnInit, Optional } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormControl, FormGroupDirective, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { FloatLabelType } from '@angular/material/form-field';
+import { AppFormUtils, isNil, isNotNilOrNaN } from '@sumaris-net/ngx-components';
+import { filter } from 'rxjs/operators';
+import { isNilOrNaN, roundHalfUp } from '@app/shared/functions';
+
+const noop = () => {
+};
+
+export declare type SamplingRatioFormat = '%'|'1/w';
+
+export const DEFAULT_MAX_DECIMALS = 6;
+
+@Component({
+  selector: 'mat-sampling-ratio-field',
+  templateUrl: './material.sampling-ratio.html',
+  styleUrls: ['./material.sampling-ratio.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => MatSamplingRatioField),
+    }
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MatSamplingRatioField implements OnInit, OnDestroy, ControlValueAccessor {
+  private _onChangeCallback: (_: any) => void = noop;
+  private _onTouchedCallback: () => void = noop;
+  private _subscription = new Subscription();
+  private _disabling = false;
+  private _writing = false;
+
+  _readonly = false;
+  _inputFormControl: FormControl;
+  _inputMaxDecimals: number;
+  _pattern: string;
+  _placeholder: string;
+  _format: SamplingRatioFormat = '%';
+
+  get disabled(): any {
+    return this.readonly || this.formControl.disabled;
+  }
+
+  @Input() formControl: FormControl;
+  @Input() formControlName: string;
+  @Input() required = false;
+  @Input() floatLabel: FloatLabelType = 'auto';
+  @Input() tabindex: number;
+  @Input() maxDecimals: number = DEFAULT_MAX_DECIMALS;
+  @Input('class') classList: string;
+
+  @Input() set readonly(value: boolean){
+    if (this._readonly !== value) {
+      this._readonly = value;
+      this.markForCheck();
+    }
+  }
+
+  get readonly(): boolean {
+    return this._readonly;
+  }
+
+  @Input() set placeholder(value: string) {
+    if (this._placeholder !== value) {
+      this._placeholder = value;
+      this.markForCheck();
+    }
+  }
+
+  get placeholder(): string {
+    return this._placeholder
+      || (this._format === '1/w' ? 'TRIP.BATCH.EDIT.SAMPLING_RATIO_PCT' : 'TRIP.BATCH.EDIT.SAMPLING_RATIO_PCT');
+  }
+
+  @Input() set format(value: SamplingRatioFormat) {
+    if (this._format !== value) {
+      this._format = value;
+      this.updatePatternAndMaxDecimals();
+      this.markForCheck();
+    }
+  }
+
+  get format(): SamplingRatioFormat {
+    return this._format;
+  }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private cd: ChangeDetectorRef,
+    @Optional() private formGroupDir: FormGroupDirective
+  ) {
+  }
+
+  ngOnInit() {
+    this.format = this.format || '%';
+
+    if (isNil(this.maxDecimals)) {
+      this.maxDecimals = DEFAULT_MAX_DECIMALS;
+    }
+    else if (this.maxDecimals < 0) {
+      console.error('Invalid attribute \'maxDecimals\'. Must a positive value.');
+      this.maxDecimals = DEFAULT_MAX_DECIMALS;
+    }
+
+    this.formControl = this.formControl || this.formControlName && this.formGroupDir && this.formGroupDir.form.get(this.formControlName) as FormControl;
+    if (!this.formControl) throw new Error('Missing mandatory attribute \'formControl\' or \'formControlName\' in <mat-latlong-field>.');
+
+    this._inputFormControl = this.formBuilder.control([null]);
+    this.updatePatternAndMaxDecimals();
+
+    this._subscription.add(
+        this._inputFormControl.valueChanges
+        .subscribe((value) => this.onFormChange(value))
+    );
+
+    // Listen status changes (when done outside the component  - e.g. when setErrors() is calling on the formControl)
+    this._subscription.add(
+      this.formControl.statusChanges
+        .pipe(
+          filter((_) => !this._readonly && !this._writing && !this._disabling) // Skip
+        )
+        .subscribe((_) => this.markForCheck())
+    );
+  }
+
+  ngOnDestroy() {
+    this._subscription.unsubscribe();
+  }
+
+  writeValue(obj: any): void {
+    if (this._writing) return;
+
+    const value = (typeof obj === 'string') ? parseFloat(obj.replace(/,/g, '.')) : obj;
+    this._writing = true;
+
+    let formValue: number = this.toFormValue(value);
+
+    // DEBUG
+    console.debug("[mat-sampling-ratio] formValue: " + formValue);
+
+    this._inputFormControl.patchValue(formValue, {emitEvent: false});
+    this._writing = false;
+    this.markForCheck();
+  }
+
+
+  registerOnChange(fn: any): void {
+    this._onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this._onTouchedCallback = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    if (this._disabling) return;
+
+    this._disabling = true;
+    if (isDisabled) {
+      this._inputFormControl.disable({onlySelf: true, emitEvent: false});
+    } else {
+      this._inputFormControl.enable({onlySelf: true, emitEvent: false});
+    }
+    this._disabling = false;
+    this.markForCheck();
+  }
+
+  displayValue(modelValue: number): string {
+    let formValue: number = this.toFormValue(modelValue);
+    if (isNotNilOrNaN(modelValue)) {
+      switch (this._format) {
+        case '1/w':
+          return `1/${formValue}`;
+        case '%':
+        default:
+          return ''+formValue;
+      }
+    }
+    return '';
+  }
+
+  private updatePatternAndMaxDecimals() {
+    switch (this._format) {
+      case '1/w':
+        this._inputMaxDecimals = Math.max(0, this.maxDecimals - 2);
+        const ngDigits = Math.max(3, this.maxDecimals);
+        this._pattern = `[0-9]{1,${ngDigits}([.][0-9]{0,${this._inputMaxDecimals}})?`;
+        break;
+      case '%':
+      default:
+        // max 2 decimals
+        this._inputMaxDecimals = Math.min(2, Math.max(0, this.maxDecimals - 2));
+        this._pattern = `(100|[0-9]{1,2}([.][0-9]{0,${this._inputMaxDecimals}})?)`;
+        break;
+    }
+  }
+
+  private onFormChange(value: number): void {
+    if (this._writing) return; // Skip if call by self
+    this._writing = true;
+
+    if (this._inputFormControl.invalid ) {
+      this.formControl.markAsPending();
+      this.formControl.setErrors({
+        ...this.formControl.errors,
+        ...this._inputFormControl.errors
+      });
+      this._writing = false;
+      this.checkIfTouched();
+      return;
+    }
+
+    let modelValue: number = null;
+    if (isNotNilOrNaN(value)) {
+      switch (this._format) {
+        case '1/w':
+          modelValue = roundHalfUp(1 / value, this.maxDecimals);
+          break;
+        case '%':
+        default:
+          modelValue = Math.min(1, roundHalfUp(value / 100, this.maxDecimals));
+          break;
+      }
+    }
+    // DEBUG
+    console.debug('[mat-sampling-ratio] modelValue=', modelValue);
+
+    // Set model value
+    this.emitChange(modelValue);
+    this._writing = false;
+  }
+
+  private emitChange(value: number) {
+    if (this.formControl.value !== value) {
+
+      // DEBUG
+      console.debug('[mat-sampling-ratio] Emit new value: ' + value);
+
+      // Changes comes from inside function: use the callback
+      this._onChangeCallback(value);
+
+      // Check if need to update controls
+      this.checkIfTouched();
+    }
+  }
+
+  private checkIfTouched(): boolean {
+    if (this.formControl.touched || this._inputFormControl.touched) {
+      this.markForCheck();
+      this._onTouchedCallback();
+      return true;
+    }
+    return false;
+  }
+
+  private toFormValue(value: number) : number {
+    if (isNilOrNaN(value)) return null;
+    switch (this.format) {
+      case '1/w':
+        return roundHalfUp(1 / value, this._inputMaxDecimals);
+      case '%':
+      default:
+        return Math.min(100, roundHalfUp(value * 100, this._inputMaxDecimals));
+    }
+  }
+
+  private markForCheck() {
+    this.cd.markForCheck();
+  }
+
+  selectInputContent = AppFormUtils.selectInputContent;
+}
