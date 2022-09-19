a071786e2af1a20355147ac47dd4c82b38f1061b
Indelog <maxime@indelog.fr>
2022-09-19 17:15:40 +0200

    [fix] SplitArrayInChunksPipe nbPeerRow number verification

--- src/app/shared/pipes/arrays.pipe.ts
+++ src/app/shared/pipes/arrays.pipe.ts
@@ -5,15 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SplitArrayInChunksPipe implements PipeTransform {
 
-  transform<T>(val: T[], _nbPeerRow: number): Array<Array<T>> {
-    if (! _nbPeerRow || _nbPeerRow < 1) {
-      throw 'Number of row must be a positive number !';
+  transform<T>(val: T[], nbPeerRow: number): Array<Array<T>> {
+    if (!nbPeerRow || isNaN(nbPeerRow) || nbPeerRow < 1) {
+      throw '[splitArrayInChunks] Number of row must be a positive number !';
     }
-    const finalChunk = Math.floor(val.length / _nbPeerRow) + 1;
+    const finalChunk = Math.floor(val.length / nbPeerRow) + 1;
     let res = new Array(finalChunk);
-    for (let i = 0 ; i < finalChunk ; i++) {
-      let begin = i * _nbPeerRow;
-      let ending = i+1 * _nbPeerRow;
+    for (let i = 0; i < finalChunk; i++) {
+      let begin = i * nbPeerRow;
+      let ending = i + 1 * nbPeerRow;
       res[i] = val.slice(begin, ending);
     }
     return res;
