d9ef0e5e23f0ad6164239a615487dd8fc44cdaa8
DEMAREST Maxime <maxime@indelog.fr>
2022-10-05 12:02:19 +0200

    [wip] TripReport : component + route

---
+++ src/app/trip/trip/report/trip.report.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TripReport } from './trip.report';
+
+describe('TripReport', () => {
+  let component: TripReport;
+  let fixture: ComponentFixture<TripReport>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TripReport ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TripReport);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
