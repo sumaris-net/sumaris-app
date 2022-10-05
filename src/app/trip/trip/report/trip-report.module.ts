7d3c876d02b6358aeb24bd1ecff36d89a0123489
DEMAREST Maxime <maxime@indelog.fr>
2022-10-05 11:39:25 +0200

    [wip] TripReport : Basics for module

---
+++ src/app/trip/trip/report/trip-report.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppCoreModule } from '@app/core/core.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    AppCoreModule,
+  ],
+  exports: [],
+})
+export class TripReportModule { }
