83a8c04ca3840bd7b34b96c4b7c27f2ee33afea4
DEMAREST Maxime <maxime@indelog.fr>
2022-10-06 11:17:38 +0200

    [wip] TripReport(selectivity) : basics for slide

--- src/app/trip/operation/selectivity/report/selectivity-operation.report.ts
+++ src/app/trip/operation/selectivity/report/selectivity-operation.report.ts
@@ -1,17 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { AppRootDataReport } from '@app/data/report/root-data-report.class';
+import { Trip } from '@app/trip/services/model/trip.model';
+import { TripService } from '@app/trip/services/trip.service';
 
 @Component({
   selector: 'app-selectivity-operation-report',
   templateUrl: './selectivity-operation.report.html',
   styleUrls: ['./selectivity-operation.report.scss']
 })
-export class SelectivityOperationReport implements OnInit {
+export class SelectivityOperationReport extends AppRootDataReport<Trip> {
 
-  constructor() {
-    console.debug('IM LOADED');
+  private dataService: TripService;
+
+  constructor(injector: Injector) {
+    super(injector);
+    this.dataService = injector.get(TripService);
   }
 
-  ngOnInit(): void {
+  protected async loadData(id: number): Promise<Trip> {
+    console.debug(`[${this.constructor.name}.loadData]`, arguments);
+    return await this.dataService.load(id);
+  }
+
+  protected computeDefaultBackHref(data: Trip): string {
+    console.debug(`[${this.constructor.name}.computeDefaultBackHref]`, arguments);
+    return `/trips/${data.id}?tab=1`;
+  }
+
+  protected async computeTitle(data: Trip): Promise<string> {
+    console.debug(`[${this.constructor.name}.computeTitle]`, arguments);
+    const title = await this.translate.get('TRIP.REPORT.TITLE', {
+      departureDate: this.dateFormatPipe.transform(data.departureDateTime, {time: false}),
+      vessel: data.vesselSnapshot.exteriorMarking,
+    }).toPromise();
+    return title;
   }
 
 }
