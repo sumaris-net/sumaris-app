aae6267dfc3051c2607fed49244d524be4387c1a
Benoit Lavenier <benoit.lavenier@e-is.pro>
2022-12-16 16:48:05 +0100

    [fix] Fix operation enable state (update ngx-components)

--- src/zone-flags.ts
+++ src/zone-flags.ts
@@ -2,5 +2,13 @@
  * Prevents Angular change detection from
  * running with certain Web Component callbacks
  */
-// eslint-disable-next-line no-underscore-dangle
-(window as any).__Zone_disable_customElements = true;
+import {zoneConfig} from '@rx-angular/cdk/zone-configurations';
+
+//(window as any).__Zone_disable_customElements = true;
+zoneConfig.global.disable.customElements();
+
+// FIXME progress bar freeze
+//zoneConfig.global.disable.timers();
+
+// TODO
+//zoneConfig.events.disable.UNPATCHED_EVENTS(['moueover', 'mousemove']);
