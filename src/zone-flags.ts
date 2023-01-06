fc1dbb965c9af55205908cd357fa37fbec893c7f
Benoit Lavenier <benoit.lavenier@e-is.pro>
2023-01-06 16:08:15 +0100

    [fix] Add code comment

--- src/zone-flags.ts
+++ src/zone-flags.ts
@@ -10,14 +10,19 @@ zoneConfig.global.disable.geolocation();
 zoneConfig.global.disable.canvas();
 zoneConfig.global.disable.XHR();
 
-// FIXME: need to patch progression toolbar, to call markForCheck()
-// Otherwise, the trip editor still show the loading bar
-//zoneConfig.global.disable.timers();
+zoneConfig.events.disable.UNPATCHED_EVENTS([
+  'mousemove', 'mouseover',
+  // TODO: check if can disabled this events:
+  //'scroll'
+]);
 
 // FIXME disable zone in .then() functions
 //zoneConfig.global.disable.ZoneAwarePromise();
 
-// FIXME: check if can disabled this events
-zoneConfig.events.disable.UNPATCHED_EVENTS(['mousemove', 'mouseover']);
+// FIXME: need to patch progression toolbar, to call markForCheck()
+// Otherwise, the trip editor still show the loading bar
+//zoneConfig.global.disable.timers();
+
+
 
 
