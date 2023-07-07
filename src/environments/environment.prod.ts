92f1101e841a02759749541086ac6dbc5934ec73
Benoit Lavenier <benoit.lavenier@e-is.pro>
2023-07-07 20:42:14 +0200

    Upgrade min peer version to 2.5.0 + i18n EN

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ export const environment = Object.freeze(<AppEnvironment>{
   apolloFetchPolicy: "cache-first",
 
   // Must be change manually. Can be override using Pod properties 'sumaris.app.min.version'
-  peerMinVersion: '2.4.3',
+  peerMinVersion: '2.5.0',
 
   // Check Web new app version
   checkAppVersionIntervalInSeconds: 5 * 60, // every 5min
