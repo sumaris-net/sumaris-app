63db9f8e85a96374806d77e22b15e3f1693b0e3a
Benoit Lavenier <benoit.lavenier@e-is.pro>
2023-10-24 18:32:56 +0200

    [enh] Trip page: add sub menu items, using tabs [enh] Operation page: add sub menu items, using tabs

--- src/app/trip/trip/trip.module.ts
+++ src/app/trip/trip/trip.module.ts
@@ -1,22 +1,23 @@
-import {NgModule} from '@angular/core';
-import {TripTable} from './trips.table';
-import {TripPage} from './trip.page';
-import {TripForm} from './trip.form';
-import {AppReferentialModule} from '@app/referential/referential.module';
-import {AppDataModule} from '@app/data/data.module';
-import {TranslateModule} from '@ngx-translate/core';
-import {CommonModule} from '@angular/common';
-import {TripTrashModal} from './trash/trip-trash.modal';
-import {AppCoreModule} from '@app/core/core.module';
-import {TripOfflineModal} from '@app/trip/trip/offline/trip-offline.modal';
-import {A11yModule} from '@angular/cdk/a11y';
-import {VesselModule} from '@app/vessel/vessel.module';
-import {AppMeasurementModule} from '@app/data/measurement/measurement.module';
-import {AppPhysicalGearModule} from '@app/trip/physicalgear/physical-gear.module';
-import {AppOperationModule} from '@app/trip/operation/operation.module';
-import {AppSaleModule} from '@app/trip/sale/sale.module';
-import {AppSocialModule} from '@app/social/social.module';
+import { NgModule } from '@angular/core';
+import { TripTable } from './trips.table';
+import { TripPage } from './trip.page';
+import { TripForm } from './trip.form';
+import { AppReferentialModule } from '@app/referential/referential.module';
+import { AppDataModule } from '@app/data/data.module';
+import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { TripTrashModal } from './trash/trip-trash.modal';
+import { AppCoreModule } from '@app/core/core.module';
+import { TripOfflineModal } from '@app/trip/trip/offline/trip-offline.modal';
+import { A11yModule } from '@angular/cdk/a11y';
+import { VesselModule } from '@app/vessel/vessel.module';
+import { AppMeasurementModule } from '@app/data/measurement/measurement.module';
+import { AppPhysicalGearModule } from '@app/trip/physicalgear/physical-gear.module';
+import { AppOperationModule } from '@app/trip/operation/operation.module';
+import { AppSaleModule } from '@app/trip/sale/sale.module';
+import { AppSocialModule } from '@app/social/social.module';
 import { AppExtractionButtonModule } from '@app/extraction/button/extraction-button.module';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   imports: [
@@ -38,23 +39,17 @@ import { AppExtractionButtonModule } from '@app/extraction/button/extraction-but
     AppPhysicalGearModule,
     AppOperationModule,
     AppSaleModule,
+    MatTabsModule,
   ],
-  declarations: [
-    TripTable,
-    TripForm,
-    TripPage,
-    TripTrashModal,
-    TripOfflineModal
-  ],
+  declarations: [TripTable, TripForm, TripPage, TripTrashModal, TripOfflineModal],
   exports: [
     // Components
     TripTable,
     TripPage,
-    TripForm
-  ]
+    TripForm,
+  ],
 })
 export class AppTripModule {
-
   constructor() {
     console.debug('[trip] Creating module...');
   }
