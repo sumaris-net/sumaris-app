a310f86596edc9c52d7d8fb99afe2ad8e942250a
Benoit Lavenier <benoit.lavenier@e-is.pro>
2023-12-20 11:00:52 +0100

    Revert "[enh] Add obsmer.sumaris.net to into peers list"
    This reverts commit 371cbc18d8bc4044b419a17cee880a4995f10669.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ export const environment = Object.freeze(<AppEnvironment>{
   apolloFetchPolicy: 'cache-first',
 
   // Must be change manually. Can be override using Pod properties 'sumaris.app.min.version'
-  peerMinVersion: '2.9.0',
+  peerMinVersion: '2.6.0',
 
   // Check Web new app version
   checkAppVersionIntervalInSeconds: 5 * 60, // every 5min
@@ -58,10 +58,6 @@ export const environment = Object.freeze(<AppEnvironment>{
     },
     {
       host: 'test.sumaris.net',
-      port: 443
-    },
-    {
-      host: 'obsmer.sumaris.net',
       port: 443
     }
   ],
