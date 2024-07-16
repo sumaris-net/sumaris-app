0e8a6920e4482ee0dd7ad37fd067b3c80925bcca
Morgan Facorat <m.facorat@codra.fr>
2024-07-16 11:29:00 +0200

    enh(calendar) Add import feature in calendar page

--- src/app/vessel/services/errors.ts
+++ src/app/vessel/services/errors.ts
@@ -2,3 +2,8 @@ export const VesselErrorCodes = {
   REPLACE_VESSEL_ERROR: 220,
   SIOP_IMPORT_ERROR: 221,
 };
+
+export const ActivityCalendarErrorCodes = {
+  REPLACE_ACTIVITY_CALENDAR_ERROR: 220,
+  SIOP_IMPORT_ERROR: 221,
+};
