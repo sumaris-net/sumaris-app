4fd880d84bcaa70ff3e0dcc2d4b289d8de658937
DEMAREST <maxime.demarest@e-is.pro>
2024-08-09 08:12:36 +0000

    Resolve "[Actiflot] [Rapport] Formulaire de terrain vierge inaccessible"
    Closes #677

--- src/app/activity-calendar/report/activity-calendar-report-routing.module.ts
+++ src/app/activity-calendar/report/activity-calendar-report-routing.module.ts
@@ -1,15 +1,23 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { FormActivityCalendarReportModule } from './form/form-activity-calendar-report.module';
+import { FormActivityCalendarReport } from './form/form-activity-calendar.report';
 
 const routes: Routes = [
   {
     path: 'form',
-    loadChildren: () => import('./form/form-activity-calendar-routing.module').then((m) => m.FormActivityCalendarRoutingModule),
+    pathMatch: 'full',
+    component: FormActivityCalendarReport,
+  },
+  {
+    path: 'blank-form',
+    pathMatch: 'full',
+    component: FormActivityCalendarReport,
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), FormActivityCalendarReportModule],
   exports: [RouterModule],
 })
 export class ActivityCalendarReportRoutingModule {}
