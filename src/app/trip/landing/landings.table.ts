2d75884478fdcd6dfa463349e703e74e822563c3
Jean-Victor FAMY <jv.famy@codra.fr>
2024-08-27 17:07:56 +0200

    fix(landings) Prevent selecting the same species twice for PETS

--- src/app/trip/landing/landings.table.ts
+++ src/app/trip/landing/landings.table.ts
@@ -109,6 +109,7 @@ export class LandingsTable
   protected dividerPmfm: IPmfm;
   protected statusList = DataQualityStatusList.filter((s) => s.id !== DataQualityStatusIds.VALIDATED);
   protected statusById = DataQualityStatusEnum;
+  protected excludedTaxonGroups: number[] = [];
   @RxStateProperty() protected observedCount: number;
   @RxStateProperty() protected availableTaxonGroups: TaxonGroupRef[];
 
@@ -424,6 +425,13 @@ export class LandingsTable
   onPrepareRowForm(form: UntypedFormGroup) {
     // Update measurement form
     if (this.isSaleDetailEditor) {
+      // Get taxon groups to exclude (already existing)
+      this.excludedTaxonGroups = this.dataSource
+        .getRows()
+        .filter((row) => this.isLandingPets(row))
+        .filter((row) => !!row.currentData.measurementValues[PmfmIds.TAXON_GROUP_ID])
+        .map((row) => row.currentData.measurementValues[PmfmIds.TAXON_GROUP_ID].id);
+
       this.validatorService.updateFormGroup(form, { pmfms: this.pmfms });
     }
 
