598b320dd1523972ecca2ccd09ab348b4166cc82
Morgan Facorat <m.facorat@codra.fr>
2024-09-02 11:18:02 +0200

    fix: lignes de plan > taxonNames multiple

--- src/app/referential/services/filter/strategy.filter.ts
+++ src/app/referential/services/filter/strategy.filter.ts
@@ -1,4 +1,5 @@
 import {
+  Department,
   EntityAsObjectOptions,
   EntityClass,
   EntityUtils,
@@ -14,7 +15,8 @@ import {
 import { BaseReferentialFilter } from '@app/referential/services/filter/referential.filter';
 import { AppliedPeriod, Strategy } from '@app/referential/services/model/strategy.model';
 import { Moment } from 'moment';
-import { TaxonNameRef } from '@app/referential/services/model/taxon-name.model';
+import { TaxonName, TaxonNameRef } from '@app/referential/services/model/taxon-name.model';
+import { ObservedLocation } from '@app/trip/observedlocation/observed-location.model';
 
 @EntityClass({ typename: 'StrategyFilterVO' })
 export class StrategyFilter extends BaseReferentialFilter<StrategyFilter, Strategy> {
@@ -23,8 +25,11 @@ export class StrategyFilter extends BaseReferentialFilter<StrategyFilter, Strate
   startDate: Moment;
   endDate: Moment;
   department: ReferentialRef;
+  departments?: ReferentialRef[];
   location: ReferentialRef;
+  locations: ReferentialRef[];
   taxonName: TaxonNameRef;
+  taxonNames: TaxonNameRef[];
   analyticReference: ReferentialRef;
 
   parameterIds?: number[];
@@ -38,8 +43,11 @@ export class StrategyFilter extends BaseReferentialFilter<StrategyFilter, Strate
     this.startDate = fromDateISOString(source.startDate);
     this.endDate = fromDateISOString(source.endDate);
     this.department = (source.department && ReferentialRef.fromObject(source.department)) || undefined;
+    this.departments = (source.departments && source.departments.map(Department.fromObject)) || [];
     this.location = (source.location && ReferentialRef.fromObject(source.location)) || undefined;
+    this.locations = (source.locations && source.locations.map(ObservedLocation.fromObject)) || [];
     this.taxonName = (source.taxonName && TaxonNameRef.fromObject(source.taxonName)) || undefined;
+    this.taxonNames = (source.taxonNames && source.taxonNames.map(TaxonName.fromObject)) || [];
     this.analyticReference = (source.analyticReference && ReferentialRef.fromObject(source.analyticReference)) || undefined;
 
     this.parameterIds = source.parameterIds;
@@ -57,12 +65,28 @@ export class StrategyFilter extends BaseReferentialFilter<StrategyFilter, Strate
 
     if (opts && opts.minify) {
       target.departmentIds = ReferentialUtils.isNotEmpty(this.department) ? [this.department.id] : undefined;
-      target.locationIds = ReferentialUtils.isNotEmpty(this.location) ? [this.location.id] : undefined;
-      target.referenceTaxonIds = EntityUtils.isNotEmpty(this.taxonName, 'referenceTaxonId') ? [this.taxonName.referenceTaxonId] : undefined;
+      target.departmentIds = ReferentialUtils.isNotEmpty(this.department)
+        ? ReferentialUtils.isNotEmpty(this.department)
+          ? [this.department.id]
+          : undefined
+        : (this.departments && this.departments.map((o) => o && o.id).filter(isNotNil)) || undefined;
+      target.locationIds = ReferentialUtils.isNotEmpty(this.location)
+        ? ReferentialUtils.isNotEmpty(this.location)
+          ? [this.location.id]
+          : undefined
+        : (this.locations && this.locations.map((o) => o && o.id).filter(isNotNil)) || undefined;
+      target.referenceTaxonIds = EntityUtils.isNotEmpty(this.taxonName, 'referenceTaxonId')
+        ? EntityUtils.isNotEmpty(this.taxonName, 'referenceTaxonId')
+          ? [this.taxonName.referenceTaxonId]
+          : undefined
+        : (this.taxonNames && this.taxonNames.map((o) => o && o.id).filter(isNotNil)) || undefined;
       target.analyticReferences = EntityUtils.isNotEmpty(this.analyticReference, 'label') ? [this.analyticReference.label] : undefined;
       delete target.department;
+      delete target.departments;
       delete target.location;
+      delete target.locations;
       delete target.taxonName;
+      delete target.taxonNames;
       delete target.analyticReference;
       delete target.programId;
       delete target.acquisitionLevel;
@@ -99,17 +123,34 @@ export class StrategyFilter extends BaseReferentialFilter<StrategyFilter, Strate
     if (isNotNil(referenceTaxonId)) {
       filterFns.push((t) => t.taxonNames && t.taxonNames.some((tns) => tns.taxonName?.referenceTaxonId === referenceTaxonId));
     }
+    if (this.taxonNames) {
+      const referenceTaxonIds = this.taxonNames && this.taxonNames.map((o) => o && o.id).filter(isNotNil);
+      if (isNotEmptyArray(referenceTaxonIds)) {
+        filterFns.push((t) => t.taxonNames && t.taxonNames.some((tns) => tns.taxonName?.referenceTaxonId === referenceTaxonId));
+      }
+    }
 
     // Department
     const departmentId = this.department?.id;
     if (isNotNil(departmentId)) {
       filterFns.push((t) => t.departments && t.departments.some((d) => d.id === departmentId));
     }
-
+    if (this.departments) {
+      const departmentIds = this.departments && this.departments.map((o) => o && o.id).filter(isNotNil);
+      if (isNotEmptyArray(departmentIds)) {
+        filterFns.push((t) => t.departments && t.departments.findIndex((o) => o && departmentIds.includes(o.id)) !== -1);
+      }
+    }
     // Location
     const locationId = this.location?.id;
     if (isNotNil(locationId)) {
       filterFns.push((t) => t.appliedStrategies && t.appliedStrategies.some((as) => as.location?.id === locationId));
+    }
+    if (this.locations) {
+      const locationIds = this.locations && this.locations.map((o) => o && o.id).filter(isNotNil);
+      if (isNotEmptyArray(locationIds)) {
+        filterFns.push((t) => t.appliedStrategies && t.appliedStrategies.findIndex((o) => o && locationIds.includes(o.id)) !== -1);
+      }
     }
 
     // Analytic reference
