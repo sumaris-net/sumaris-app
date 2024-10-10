bda674b1166125a72d5374fcfb6b89a0bd457106
Benoit Lavenier <benoit.lavenier@e-is.pro>
2024-10-10 14:23:46 +0200

    fix(entity-quality-form): fix terminate/control error object, used to call editor.setError()

--- src/app/data/quality/entity-quality-form.component.ts
+++ src/app/data/quality/entity-quality-form.component.ts
@@ -185,7 +185,7 @@ export class EntityQualityFormComponent<
         if (isNotNil(errors.details)) {
           errors = <AppErrorWithDetails>{
             message: errors.message || data.qualificationComments || 'COMMON.FORM.HAS_ERROR',
-            details: { errors: errors.details as FormErrors },
+            details: { errors: errors.details.errors as FormErrors },
           };
         } else {
           errors.message = errors.message || data.qualificationComments || 'COMMON.FORM.HAS_ERROR';
