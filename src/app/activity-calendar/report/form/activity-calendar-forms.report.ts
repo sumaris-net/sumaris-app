7eae97698a51cda9da22cb769c022faaef0f9ea8
DEMAREST Maxime <maxime@indelog.fr>
2024-10-24 12:07:57 +0200

    fix(ActivityCalendrarFormsReport) multi-report with table filter

--- src/app/activity-calendar/report/form/activity-calendar-forms.report.ts
+++ src/app/activity-calendar/report/form/activity-calendar-forms.report.ts
@@ -112,9 +112,7 @@ export class ActivityCalendarFormsReport extends AppBaseReport<ActivityCalendar[
     return result;
   }
 
-  protected async loadData(ids: number[], opts?: any): Promise<ActivityCalendar[]> {
-    // isEmptyArray(ids) ? :
-    const filter = isEmptyArray(ids) ? this.restoreLastTableFilter() : ActivityCalendarFilter.fromObject({ includedIds: ids });
+  protected async loadData(filter: ActivityCalendarFilter, opts?: any): Promise<ActivityCalendar[]> {
     const result = [];
     const size = 500;
 
