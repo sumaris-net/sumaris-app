9dd091d42c1c397a3c4a1c3ffc5a48b5fe481d0c
Morgan Facorat <m.facorat@codra.fr>
2024-11-12 14:09:29 +0100

    enh(observations-sale) add modal

--- src/app/trip/batch/sub/sub-batches.modal.ts
+++ src/app/trip/batch/sub/sub-batches.modal.ts
@@ -38,7 +38,18 @@ import { ModalUtils } from '@app/shared/modal/modal.utils';
 import { AbstractControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { PmfmUtils } from '@app/referential/services/model/pmfm-utils';
 import { MenuCloseReason } from '@angular/material/menu';
+import { SubSortingCriteriaModal } from './sub-sorting-criteria.modal';
 
+export interface SubBatchSortingCriteria {
+  scientificSpecies: TaxonNameRef;
+  sortingCriteria: string; // dan sle noveau d'acquisition
+  method: string;
+  unit: string;
+  min: number;
+  max: number;
+  measurementPrecision: number;
+  analysisInstrument: string;
+}
 export interface ISubBatchesModalOptions {
   disabled: boolean;
   showParentGroup: boolean;
@@ -736,6 +747,29 @@ export class SubBatchesModal extends SubBatchesTable implements OnInit, ISubBatc
       this.setError((err && err.message) || err);
       this.markAsLoaded();
     }
+  }
+
+  async openSortingCriteriaModal() {
+    const modal = await this.modalCtrl.create({
+      component: SubSortingCriteriaModal,
+      backdropDismiss: false,
+      cssClass: 'modal-small',
+      componentProps: {
+        parentGroup: this.parentGroup,
+        programLabel: this.programLabel,
+      },
+    });
+    // add backdrop opacity to modal
+    modal.style.setProperty('--backdrop-opacity', '0.4');
+    // Open the modal
+    await modal.present();
+
+    const { data } = await modal.onDidDismiss();
+    if (data) {
+      console.log('SubSortingCriteriaModal data:', data);
+    }
+
+    return data;
   }
 
   getFormErrors = AppFormUtils.getFormErrors;
