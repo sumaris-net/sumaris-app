4f77b723635a93df390fb06046fd3c584bf8a009
Benoit Lavenier <benoit.lavenier@e-is.pro>
2024-11-22 15:54:52 +0100

    fix(activity-calendar) Use existing program's option to hide predoc program

--- src/app/activity-calendar/activity-calendar.config.ts
+++ src/app/activity-calendar/activity-calendar.config.ts
@@ -3,7 +3,6 @@ import { TypePolicies } from '@apollo/client/core';
 import { AcquisitionLevelCodes } from '@app/referential/services/model/model.enum';
 import { ProgramFilter } from '@app/referential/services/filter/program.filter';
 import { ActivityCalendar } from '@app/activity-calendar/model/activity-calendar.model';
-import { ReferentialRefFilter } from '@app/referential/services/filter/referential-ref.filter';
 
 /**
  * Name of the features (e.g. to be used by settings)
@@ -34,19 +33,6 @@ export const ACTIVITY_CALENDAR_CONFIG_OPTIONS = Object.freeze({
       },
     ],
     defaultValue: 'MENU.ACTIVITY_CALENDAR',
-  },
-  ACTIVITY_CALENDAR_HIDDEN_PROGRAM_IDS: <FormFieldDefinition>{
-    key: 'sumaris.activityCalendar.hidden.program.ids',
-    label: 'ACTIVITY_CALENDAR.OPTIONS.HIDDEN_PROGRAM_IDS',
-    type: 'entities',
-    autocomplete: {
-      attributes: ['label'],
-      filter: <ReferentialRefFilter>{
-        entityName: 'Program',
-        statusIds: [StatusIds.ENABLE, StatusIds.TEMPORARY],
-      },
-    },
-    defaultValue: '',
   },
 });
 
