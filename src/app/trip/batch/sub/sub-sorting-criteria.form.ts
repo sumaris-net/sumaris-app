be4eafb311497bb8acc654fc7ecc2b0c38d8d33c
Morgan Facorat <m.facorat@codra.fr>
2024-12-03 11:40:04 +0100

    wip(sub-batches): add new form (work!)

--- src/app/trip/batch/sub/sub-sorting-criteria.form.ts
+++ src/app/trip/batch/sub/sub-sorting-criteria.form.ts
@@ -1,50 +1,35 @@
-import { Component, Inject, Injector, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Injector, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { FormFieldDefinitionMap, isEmptyArray, isNil, isNotEmptyArray, LoadResult, LocalSettingsService } from '@sumaris-net/ngx-components';
+import { AppForm, FormFieldDefinitionMap, isEmptyArray, isNil, isNotEmptyArray, LoadResult, LocalSettingsService } from '@sumaris-net/ngx-components';
 import { BatchGroup } from '../group/batch-group.model';
 import { TaxonNameRef } from '@app/referential/services/model/taxon-name.model';
 import { ProgramRefService } from '@app/referential/services/program-ref.service';
-import { SUB_BATCHES_TABLE_OPTIONS, SubBatchesTable } from './sub-batches.table';
-import { SubBatchValidatorService } from './sub-batch.validator';
-import { SubBatch } from './sub-batch.model';
-import { APP_MAIN_CONTEXT_SERVICE, ContextService } from '@app/shared/context.service';
 import { RxState } from '@rx-angular/state';
 import { PmfmService } from '@app/referential/services/pmfm.service';
 import { IPmfm, Pmfm, PmfmUtils } from '@app/referential/services/model/pmfm.model';
 import { DenormalizedPmfmStrategy } from '@app/referential/services/model/pmfm-strategy.model';
-import { MeasurementValuesForm } from '@app/data/measurement/measurement-values.form.class';
-import { SubBatchFormState } from './sub-batch.form';
 import { MeasurementsValidatorService } from '@app/data/measurement/measurement.validator';
 
+export interface SubSortingCriteria {
+  taxonName: TaxonNameRef;
+  criteriaPmfm: IPmfm;
+  min: number;
+  max: number;
+  precision: number;
+  qvPmfm: IPmfm;
+  useOptionalCriteria: boolean;
+}
 @Component({
   selector: 'app-sub-sorting-criteria-form',
   styleUrls: ['sub-sorting-criteria.form.scss'],
   templateUrl: 'sub-sorting-criteria.form.html',
-  providers: [
-    {
-      provide: ContextService,
-      deps: [APP_MAIN_CONTEXT_SERVICE],
-    },
-    { provide: SubBatchValidatorService, useClass: SubBatchValidatorService },
-    {
-      provide: SUB_BATCHES_TABLE_OPTIONS,
-      useFactory: (settings: LocalSettingsService) => ({
-        prependNewElements: settings.mobile,
-        suppressErrors: true,
-      }),
-      deps: [LocalSettingsService],
-    },
-    RxState,
-  ],
+  providers: [RxState],
 })
-export class SubSortingCriteriaForm extends MeasurementValuesForm<SubBatch, SubBatchFormState> implements OnInit, OnDestroy {
-  protected readonly minimumValue = 0;
-  protected readonly maximumValue = 100;
+export class SubSortingCriteriaForm extends AppForm<SubSortingCriteria> implements OnInit, OnDestroy {
   protected fieldDefinitions: FormFieldDefinitionMap = {};
   protected sortingCriteriaForm: FormGroup;
   protected criteriaPmfms: IPmfm[];
   protected qvPmfms: IPmfm[];
-  // protected pmfmsFiltered: IPmfm[];
   protected isMandatoryQvPmfm: boolean = false;
   protected disabledPrecision: boolean = false;
   protected showQvPmfm: boolean = false;
@@ -61,20 +46,21 @@ export class SubSortingCriteriaForm extends MeasurementValuesForm<SubBatch, SubB
     protected programRefService: ProgramRefService,
     private fb: FormBuilder
   ) {
-    super(injector, measurementsValidatorService, fb, programRefService);
+    super(
+      injector,
+      fb.group({
+        taxonName: ['', Validators.required],
+        criteriaPmfm: ['', Validators.required],
+        min: ['', [Validators.required, Validators.min(0)]],
+        max: ['', [Validators.required, Validators.max(100)]],
+        precision: ['', [Validators.required]],
+        qvPmfm: [''],
+        useOptionalCriteria: [false],
+      })
+    );
   }
 
   async ngOnInit() {
-    this.sortingCriteriaForm = this.fb.group({
-      taxonName: ['', Validators.required],
-      criteriaPmfm: ['', Validators.required],
-      min: ['', [Validators.required, Validators.min(this.minimumValue)]],
-      max: ['', [Validators.required, Validators.max(this.maximumValue)]],
-      precision: ['', [Validators.required]],
-      qvPmfm: [''],
-      useOptionalCriteria: [false],
-    });
-
     // Pmfms filtered by type
     this.qvPmfms = this.pmfmsFiltered.filter((pmfm) => PmfmUtils.isQualitative(pmfm));
     this.criteriaPmfms = this.pmfmsFiltered.filter((pmfm) => !PmfmUtils.isQualitative(pmfm));
@@ -87,9 +73,9 @@ export class SubSortingCriteriaForm extends MeasurementValuesForm<SubBatch, SubB
       .some((strategy) => strategy.isMandatory);
 
     this.registerSubscription(
-      this.sortingCriteriaForm.get('criteriaPmfm').valueChanges.subscribe((value) => {
+      this.form.get('criteriaPmfm').valueChanges.subscribe((value) => {
         if (value.precision) {
-          this.sortingCriteriaForm.get('precision').setValue(value.precision);
+          this.form.get('precision').setValue(value.precision);
           this.disabledPrecision = true;
         } else {
           this.disabledPrecision = false;
@@ -98,9 +84,9 @@ export class SubSortingCriteriaForm extends MeasurementValuesForm<SubBatch, SubB
     );
 
     this.registerSubscription(
-      this.sortingCriteriaForm.valueChanges.subscribe(() => {
-        if (this.sortingCriteriaForm.hasError('minMaxError')) {
-          this.sortingCriteriaForm.get('maxStep').setErrors({ minMaxError: true });
+      this.form.valueChanges.subscribe(() => {
+        if (this.form.hasError('minMaxError')) {
+          this.form.get('maxStep').setErrors({ minMaxError: true });
         }
       })
     );
@@ -149,12 +135,11 @@ export class SubSortingCriteriaForm extends MeasurementValuesForm<SubBatch, SubB
     // Fill form default values
     // Check if there is only one pmfm in the mandatory pmfms
     if (this.criteriaPmfms.length === 1) {
-      this.sortingCriteriaForm.get('criteriaPmfm').setValue(this.criteriaPmfms[0]);
-      this.markAllAsTouched();
+      this.form.get('criteriaPmfm').setValue(this.criteriaPmfms[0]);
     }
     if (isNotEmptyArray(this.qvPmfms) && this.qvPmfms.length === 1 && this.isMandatoryQvPmfm) {
       this.showQvPmfm = true;
-      this.sortingCriteriaForm.get('qvPmfm').setValue(this.qvPmfms[0]);
+      this.form.get('qvPmfm').setValue(this.qvPmfms[0]);
     } else if (isNotEmptyArray(this.qvPmfms)) {
       this.showQvPmfm = true;
     }
@@ -175,8 +160,12 @@ export class SubSortingCriteriaForm extends MeasurementValuesForm<SubBatch, SubB
     if (isEmptyArray(pmfms)) return { data: [] };
     return this.pmfmService.suggest(value, {
       searchJoin: 'parameter',
-
       includedIds: pmfms.map((pmfm) => pmfm.id),
     });
+  }
+
+  doSubmit() {
+    const formValue = this.form.value;
+    return super.doSubmit(formValue);
   }
 }
