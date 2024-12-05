7cb9e057f064d233c537b2ae733b78656e546ac1
Morgan Facorat <m.facorat@codra.fr>
2024-12-05 16:47:26 +0100

    wip(sub-batches) : for demo 06/12

--- src/app/trip/batch/sub/sub-batches.modal.ts
+++ src/app/trip/batch/sub/sub-batches.modal.ts
@@ -135,6 +135,7 @@ export class SubBatchesModal extends SubBatchesTable implements OnInit, ISubBatc
   protected virtualPmfms: DenormalizedPmfmStrategy[];
   protected pmfmsFiltered: IPmfm[];
   protected footerValues: { [key: string]: number } = {};
+  isAlreadyIndividualCount: boolean = true;
   canShowEnumeration: boolean = true;
 
   get selectedRow(): TableElement<SubBatch> {
@@ -954,13 +955,16 @@ export class SubBatchesModal extends SubBatchesTable implements OnInit, ISubBatc
 
     // Add/remove rows depending on virtual columns presence
     const formIsEmpty = AppSharedFormUtils.isEmptyForm(this.filterForm);
-    if (show && !formIsEmpty) {
-      const data = this.dataSource.getRows();
-      const groupedData = this.groupByProperty(data, PmfmIds.LENGTH_TOTAL_CM.toString());
-      await this.concatRows(groupedData);
-    } else if (!show) {
-      await this.splitRows();
+    if (this.isAlreadyIndividualCount !== show) {
+      if (show && !formIsEmpty) {
+        const data = this.dataSource.getRows();
+        const groupedData = this.groupByProperty(data, PmfmIds.LENGTH_TOTAL_CM.toString());
+        await this.concatRows(groupedData);
+      } else if (!show) {
+        await this.splitRows();
+      }
     }
+    this.isAlreadyIndividualCount = show;
   }
 
   private groupByProperty(objects: TableElement<SubBatch>[], property: string): TableElement<SubBatch>[][] {
