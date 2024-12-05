37252fbfa102c87649254297a94ea91a6f7ae319
Jean-Victor FAMY <jv.famy@codra.fr>
2024-12-05 17:39:08 +0100

    enh(landings) suggestFn to filter pmfm qualitative values

--- src/app/trip/landing/landings.table.ts
+++ src/app/trip/landing/landings.table.ts
@@ -5,6 +5,8 @@ import {
   AccountService,
   AppValidatorService,
   DateUtils,
+  EntityUtils,
+  isEmptyArray,
   isNil,
   isNotEmptyArray,
   isNotNil,
@@ -14,6 +16,7 @@ import {
   ReferentialUtils,
   removeDuplicatesFromArray,
   StatusIds,
+  suggestFromArray,
   toBoolean,
   toNumber,
   UsageMode,
@@ -84,6 +87,7 @@ export class LandingsTable
   extends BaseMeasurementsTable<Landing, LandingFilter, LandingService, LandingValidatorService, LandingsTableState>
   implements OnInit, OnDestroy
 {
+  readonly pmfmIdsMap = PmfmIds;
   /** Offset to apply to SPECIES_LIST_ORIGIN.RANDOM landings (sale editor). */
   static readonly RANDOM_LANDINGS_RANK_ORDER_OFFSET = 100;
   readonly randomLandingsRankOrderOffset = LandingsTable.RANDOM_LANDINGS_RANK_ORDER_OFFSET;
@@ -109,7 +113,7 @@ export class LandingsTable
   protected dividerPmfm: IPmfm;
   protected statusList = DataQualityStatusList.filter((s) => s.id !== DataQualityStatusIds.VALIDATED);
   protected statusById = DataQualityStatusEnum;
-  protected excludedTaxonGroups: number[] = [];
+  protected suggestTaxonGroupsFn: (value: any, filter?: any) => LoadResult<TaxonGroupRef> = null;
   @RxStateProperty() protected observedCount: number;
   @RxStateProperty() protected availableTaxonGroups: TaxonGroupRef[];
 
@@ -425,12 +429,20 @@ export class LandingsTable
   onPrepareRowForm(form: UntypedFormGroup) {
     // Update measurement form
     if (this.isSaleDetailEditor) {
-      // Get taxon groups to exclude (already existing)
-      this.excludedTaxonGroups = this.dataSource
+      // Collect existing PETS taxon groups ids
+      const existingPetsTaxonGroupIds = this.dataSource
         .getRows()
         .filter((row) => this.isLandingPets(row))
-        .filter((row) => !!row.currentData.measurementValues[PmfmIds.TAXON_GROUP_ID])
-        .map((row) => row.currentData.measurementValues[PmfmIds.TAXON_GROUP_ID].id);
+        .map((row) => row.currentData.measurementValues[PmfmIds.TAXON_GROUP_ID])
+        .filter(EntityUtils.isEntity)
+        .map((taxonGroup) => taxonGroup.id);
+
+      this.suggestTaxonGroupsFn = (value: any, filter: any) => {
+        const filteredQualitativeValues = this.availableTaxonGroups
+          .filter((qv) => !existingPetsTaxonGroupIds.includes(qv.id))
+          .filter((qv) => !(this.dividerPmfmId === PmfmIds.SPECIES_LIST_ORIGIN && StrategyUtils.isRandomSelectedTaxon(qv)));
+        return suggestFromArray(filteredQualitativeValues, value, filter);
+      };
 
       this.validatorService.updateFormGroup(form, { pmfms: this.pmfms });
     }
