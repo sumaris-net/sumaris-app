e6422266c616bab37e33545d4b2cd31db100d70b
Benoit Lavenier <benoit.lavenier@e-is.pro>
2024-12-12 18:07:21 +0100

    wip(trip-card) Allow to delete and sent trip, and select a card

--- src/app/trip/trip/card/trip-card.component.ts
+++ src/app/trip/trip/card/trip-card.component.ts
@@ -1,13 +1,15 @@
-import { booleanAttribute, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { AppColors, MatAutocompleteFieldConfig } from '@sumaris-net/ngx-components';
+import { booleanAttribute, ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AppColors, DateUtils, MatAutocompleteFieldConfig } from '@sumaris-net/ngx-components';
 import { Trip } from '@app/trip/trip/trip.model';
 
 @Component({
   selector: 'app-trip-card',
   templateUrl: './trip-card.component.html',
   styleUrls: ['./trip-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TripCardComponent implements OnInit {
+  protected readonly now = DateUtils.moment();
   @Input() value: Trip;
   @Input() color: AppColors = 'light';
   @Input() config: {
@@ -18,6 +20,8 @@ export class TripCardComponent implements OnInit {
   @Input({ transform: booleanAttribute }) showProgram = true;
   @Input({ transform: booleanAttribute }) showComments = false;
   @Input({ transform: booleanAttribute }) showUpdateDate = false;
+  @Input({ transform: booleanAttribute }) canDelete = false;
+  @Input({ transform: booleanAttribute }) canSynchronize = false;
 
   @Output() delete = new EventEmitter<Event>();
   @Output() synchronize = new EventEmitter<Event>();
