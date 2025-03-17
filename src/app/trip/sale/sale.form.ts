453bbb9bc4ae00bda2bc236c5b88e567b2861c1c
FAMY <jv.famy@codra.fr>
2025-03-17 09:32:34 +0000

    Resolve "[OBSVENTES] Vente > Ajouter un champ "Métier""
    Closes #692
    
    MR sumaris-pod : sih-public/sumaris/sumaris-pod!98
    
    * [x] Ajouter au SaleVO un champ `metiers` au même titre que `fishingAreas`
    * [x] Ajout d'une option de programme pour l'affichage du champ métier
    * [x] ~~Ajout d'une option de stratégie pour l'affichage multiple de métiers=\> en attente de la MR sih-public/sumaris/sumaris-app!229~~
    * [x] J'ai eu des soucis car le parent n'était pas le bon (trip au lieu de landing), du minify sur le métier et du data.tripId qui était effacé
    
    ---
    
    Pour tester :
    
    1. Activer les options de programme :
       1. Sortie \> Vente : Métier ?
       2. Sortie \> Activer les zones de compétences ?
    2. Se rendre sur /observations/100/sale/109
    3. Changer les métiers, zones de pêche, puis jouer avec le menu "..." en haut à droite en changeant la zone de compétence.

--- src/app/trip/sale/sale.form.ts
+++ src/app/trip/sale/sale.form.ts
@@ -3,8 +3,13 @@ import { SaleValidatorOptions, SaleValidatorService } from './sale.validator';
 import { Moment } from 'moment';
 import {
   AppForm,
+  AppFormArray,
+  IReferentialRef,
   isEmptyArray,
   isNilOrBlank,
+  isNotEmptyArray,
+  isNotNil,
+  LoadResult,
   MatAutocompleteField,
   OnReady,
   ReferentialRef,
@@ -18,9 +23,12 @@ import { VesselSnapshotService } from '@app/referential/services/vessel-snapshot
 import { Sale } from './sale.model';
 import { AcquisitionLevelCodes, LocationLevelIds } from '@app/referential/services/model/model.enum';
 import { ReferentialRefService } from '@app/referential/services/referential-ref.service';
-import { UntypedFormControl } from '@angular/forms';
+import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 import { ProgramRefService } from '@app/referential/services/program-ref.service';
 import { ReferentialRefFilter } from '@app/referential/services/filter/referential-ref.filter';
+import { FishingArea } from '@app/data/fishing-area/fishing-area.model';
+import { IExpertiseAreaProperties } from '@app/referential/expertise-area/expertise-area.model';
+import { FetchPolicy } from '@apollo/client/core';
 
 @Component({
   selector: 'app-form-sale',
@@ -32,8 +40,14 @@ export class SaleForm extends AppForm<Sale> implements OnInit, OnReady {
   private _minDate: Moment = null;
   private _required: boolean;
   private _locationSuggestLengthThreshold: number;
+  private _enableExpertiseArea = false;
+  private _expertiseAreaProperties: IExpertiseAreaProperties;
 
   protected readonly mobile = this.settings.mobile;
+  protected metierFocusIndex = -1;
+  protected fishingAreaFocusIndex = -1;
+  protected invalidMetierIds: number[] = [];
+  protected invalidFishingAreaLocationIds: number[] = [];
 
   @Input() showError = true;
   @Input() showProgram = true;
@@ -45,6 +59,29 @@ export class SaleForm extends AppForm<Sale> implements OnInit, OnReady {
   @Input() i18nSuffix: string;
   @Input() showParent = false;
   @Input() locationLevelIds: number[];
+  @Input() showMetiers = false;
+  @Input() showFishingAreas = false;
+  @Input() allowManyMetiers: boolean = null;
+
+  @Input()
+  set enableExpertiseArea(value: boolean) {
+    this._enableExpertiseArea = value;
+    this.checkDataExpertiseArea();
+  }
+
+  get enableExpertiseArea(): boolean {
+    return this._enableExpertiseArea;
+  }
+
+  @Input()
+  set expertiseAreaProperties(value: IExpertiseAreaProperties) {
+    this._expertiseAreaProperties = value;
+    this.checkDataExpertiseArea();
+  }
+
+  get expertiseAreaProperties(): IExpertiseAreaProperties {
+    return this._expertiseAreaProperties;
+  }
 
   @Input() set required(value: boolean) {
     if (this._required !== value) {
@@ -107,6 +144,14 @@ export class SaleForm extends AppForm<Sale> implements OnInit, OnReady {
   }
 
   @ViewChild('locationField', { static: false }) locationField: MatAutocompleteField;
+
+  get metiersForm() {
+    return this.form.controls.metiers as AppFormArray<ReferentialRef<any>, UntypedFormControl>;
+  }
+
+  get fishingAreasForm() {
+    return this.form?.controls.fishingAreas as AppFormArray<FishingArea, UntypedFormGroup>;
+  }
 
   constructor(
     injector: Injector,
@@ -171,10 +216,163 @@ export class SaleForm extends AppForm<Sale> implements OnInit, OnReady {
         entityName: 'SaleType',
       },
     });
+
+    // Combo: metier
+    const metierAttributes = this.settings.getFieldDisplayAttributes('qualitativeValue');
+    this.registerAutocompleteField('metier', {
+      reloadItemsOnFocus: true,
+      suggestFn: (value, filter) => this.suggestMetiers(value, filter),
+      // Default filter. A excludedIds will be add dynamically
+      filter: {
+        entityName: 'Metier',
+        statusIds: [StatusIds.TEMPORARY, StatusIds.ENABLE],
+      },
+      attributes: metierAttributes,
+      mobile: this.mobile,
+    });
+
+    // Combo: fishingAreas
+    const fishingAreaAttributes = this.settings.getFieldDisplayAttributes('fishingAreaLocation', ['label', 'name']);
+    this.registerAutocompleteField('fishingAreaLocation', {
+      reloadItemsOnFocus: true,
+      suggestFn: (value, filter) =>
+        this.suggestFishingAreaLocations(value, {
+          ...filter,
+        }),
+      filter: {
+        entityName: 'Location',
+        statusIds: [StatusIds.TEMPORARY, StatusIds.ENABLE],
+      },
+      attributes: fishingAreaAttributes,
+      showAllOnFocus: false,
+      suggestLengthThreshold: 2,
+      mobile: this.mobile,
+    });
   }
 
-  ngOnReady() {
+  async ngOnReady() {
     this.updateFormGroup();
+  }
+
+  protected buildMetierFilter(filter?: Partial<ReferentialRefFilter>, existingMetierIds?: number[]): Partial<ReferentialRefFilter> {
+    return {
+      entityName: 'Metier',
+      statusIds: [StatusIds.TEMPORARY, StatusIds.ENABLE],
+      ...filter,
+      excludedIds: existingMetierIds,
+      locationIds: this.enableExpertiseArea ? this.expertiseAreaProperties?.locationIds : filter?.locationIds,
+    };
+  }
+
+  protected buildFishingAreaFilter(filter?: Partial<ReferentialRefFilter>, existingFishingAreaLocationIds?: number[]): Partial<ReferentialRefFilter> {
+    return {
+      entityName: 'Location',
+      statusIds: [StatusIds.TEMPORARY, StatusIds.ENABLE],
+      ...filter,
+      excludedIds: existingFishingAreaLocationIds,
+      levelIds: this.enableExpertiseArea ? this.expertiseAreaProperties?.locationLevelIds : undefined,
+      locationIds: this.enableExpertiseArea ? this.expertiseAreaProperties?.locationIds : filter?.locationIds,
+    };
+  }
+
+  protected async checkDataExpertiseArea() {
+    const needCheck = this.enableExpertiseArea && isNotEmptyArray(this.expertiseAreaProperties?.locationIds);
+    const invalidMetierIds = [];
+    const invalidFishingAreaLocationIds = [];
+    const cacheFirstOptions = { fetchPolicy: <FetchPolicy>'cache-first' };
+
+    if (needCheck) {
+      // Metiers
+      for (const metierControl of this.metiersForm?.controls || []) {
+        const metierId = metierControl.value?.id;
+        if (isNotNil(metierId) && !invalidMetierIds.includes(metierId)) {
+          if (!(await this.referentialRefService.existsById(metierId, this.buildMetierFilter(), cacheFirstOptions))) {
+            invalidMetierIds.push(metierId);
+          }
+        }
+      }
+
+      // Fishing areas
+      for (const fishingAreaControl of this.fishingAreasForm?.controls || []) {
+        const faLocationId = fishingAreaControl.value.location?.id;
+        if (isNotNil(faLocationId) && !invalidFishingAreaLocationIds.includes(faLocationId)) {
+          if (!(await this.referentialRefService.existsById(faLocationId, this.buildFishingAreaFilter(), cacheFirstOptions))) {
+            invalidFishingAreaLocationIds.push(faLocationId);
+          }
+        }
+      }
+    }
+    this.invalidMetierIds = invalidMetierIds;
+    this.invalidFishingAreaLocationIds = invalidFishingAreaLocationIds;
+    this.markForCheck();
+  }
+
+  protected suggestMetiers(value: any, filter?: any): Promise<LoadResult<ReferentialRef>> {
+    const currentControlValue = ReferentialUtils.isNotEmpty(value) ? value : null;
+
+    // Excluded existing metiers, BUT keep the current control value
+    const excludedIds = (this.metiersForm.value || [])
+      .filter(ReferentialUtils.isNotEmpty)
+      .filter((item) => !currentControlValue || currentControlValue !== item)
+      .map((item) => parseInt(item.id));
+
+    return this.referentialRefService.suggest(value, this.buildMetierFilter(filter, excludedIds));
+  }
+
+  protected async suggestFishingAreaLocations(value: any, filter: any): Promise<LoadResult<IReferentialRef>> {
+    const currentControlValue: ReferentialRef = ReferentialUtils.isNotEmpty(value) ? value : null;
+    // Excluded existing locations, BUT keep the current control value
+    const excludedIds = (this.fishingAreasForm.value || <FishingArea[]>[])
+      .map((fa: FishingArea) => fa?.location)
+      .filter(ReferentialUtils.isNotEmpty)
+      .filter((item: ReferentialRef) => !currentControlValue || currentControlValue !== item)
+      .map((item: ReferentialRef) => +item.id);
+
+    return await this.referentialRefService.suggest(value, this.buildFishingAreaFilter(filter, excludedIds));
+  }
+
+  addMetier() {
+    this.metiersForm.add();
+    if (!this.mobile) {
+      this.metierFocusIndex = this.metiersForm.length - 1;
+    }
+  }
+
+  addFishingArea() {
+    this.fishingAreasForm.add();
+    if (!this.mobile) {
+      this.fishingAreaFocusIndex = this.fishingAreasForm.length - 1;
+    }
+  }
+
+  removeMetierAt(index: number) {
+    const removed = this.metiersForm.removeAt(index);
+    if (removed) {
+      this.metiersForm.markAsDirty();
+    }
+  }
+
+  async setValue(data: Sale, opts?: { emitEvent?: boolean; onlySelf?: boolean }) {
+    // Make sure to have (at least) one metier
+    this.showMetiers = this.showMetiers || isNotEmptyArray(data?.metiers);
+    if (this.showMetiers) {
+      data.metiers = data.metiers && data.metiers.length ? data.metiers : [null];
+    } else {
+      data.metiers = [];
+    }
+
+    // Make sure to have (at least) one fishing area
+    this.showFishingAreas = this.showFishingAreas || isNotEmptyArray(data?.fishingAreas);
+    if (this.showFishingAreas) {
+      data.fishingAreas = data.fishingAreas && data.fishingAreas.length ? data.fishingAreas : [null];
+    } else {
+      data.fishingAreas = [];
+    }
+
+    // Send value for form
+    super.setValue(data, opts);
+
+    await this.checkDataExpertiseArea();
   }
 
   protected updateFormGroup(opts?: { emitEvent?: boolean }) {
@@ -183,6 +381,8 @@ export class SaleForm extends AppForm<Sale> implements OnInit, OnReady {
       minDate: this._minDate,
       withProgram: this.showProgram,
       withVessel: this.showVessel,
+      withMetiers: this.showMetiers,
+      withFishingAreas: this.showFishingAreas,
     };
 
     console.info('[sale-form] Updating form group...', validatorOpts);
