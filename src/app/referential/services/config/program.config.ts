340203a69dded43a94d7bf39cd0863a9270f5adf
OMAGNE <o.magne@codra.fr>
2025-04-16 15:10:33 +0200

    enh(trip-page) param vente multiple defaut

--- src/app/referential/services/config/program.config.ts
+++ src/app/referential/services/config/program.config.ts
@@ -149,6 +149,12 @@ export const ProgramProperties = Object.freeze({
     defaultValue: 'true',
     type: 'boolean',
   },
+  TRIP_SALES_ENABLE: <FormFieldDefinition>{
+    key: 'sumaris.trip.sales.enable',
+    label: 'PROGRAM.OPTIONS.TRIP_SALES_ENABLE',
+    defaultValue: 'false',
+    type: 'boolean',
+  },
   TRIP_SALE_LOCATION_LEVEL_IDS: <FormFieldDefinition>{
     key: 'sumaris.trip.sale.location.level.ids',
     label: 'PROGRAM.OPTIONS.TRIP_SALE_LOCATION_LEVEL_IDS',
