48c4b4af5e09ede7488cbbd7fc59ea7000bea778
Benoit Lavenier <benoit.lavenier@e-is.pro>
2025-04-22 12:30:23 +0200

    fix(select-column) Fix build error, in selection pipes (upgrade to ngx-components 18.7.19)

--- src/app/shared/table/base.column.ts
+++ src/app/shared/table/base.column.ts
@@ -1,7 +1,8 @@
-import { AppAsyncTable, AppTable } from '@sumaris-net/ngx-components';
 import { ChangeDetectorRef, Directive, inject, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatColumnDef, MatTable } from '@angular/material/table';
 import { AsyncTableElement, TableElement } from '@e-is/ngx-material-table';
+import { AppBaseTable } from '@app/shared/table/base.table';
+import { AppBaseAsyncTable } from '@app/shared/table/base.async-table';
 
 export declare type AnyTableElement = TableElement<any> | AsyncTableElement<any>;
 
@@ -10,7 +11,7 @@ export abstract class BaseColumn implements OnInit, OnDestroy {
   protected matTable = inject(MatTable<any>);
   protected cd = inject(ChangeDetectorRef);
 
-  @Input() table: AppTable<any> | AppAsyncTable<any>;
+  @Input() table: AppBaseTable<any, any> | AppBaseAsyncTable<any, any>;
 
   @ViewChild(MatColumnDef) columnDef: MatColumnDef;
 
